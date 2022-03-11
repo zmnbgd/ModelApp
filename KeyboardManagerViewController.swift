26a4821fdcba11ee1bb21a370fa69a479485f3a2
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2022-03-12 00:03:11 +0100

    Add files via upload

---
+++ KeyboardManagerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  KeyboardManagerViewController.swift
+//  ModelApp
+//
+//  Created by Marko Zivanovic on 11.3.22..
+//
+
+import UIKit
+
+class KeyboardManagerViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+    
+    
+}
---
+++ WebViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WebViewController.swift
+//  ModelApp
+//
+//  Created by Marko Zivanovic on 9.3.22..
+//
+
+import UIKit
+import WebKit
+
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://google.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+
+    
+    
+}
